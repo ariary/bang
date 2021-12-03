74903495a420fc24d1e6db63e0aca4732cae28f7
Ariary <ariary9.2@hotmail.fr>
2021-12-03 16:55:27 +0100

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -15,3 +15,11 @@ mv ./bin/* ~/.local/bin/
 #fd
 sudo apt install fd-find
 ln -s $(which fdfind) ~/.local/bin/fd
+
+#go
+GO_URL="https://golang.org"
+GO_ENDPOINT="$(curl -s https://go.dev/dl/ | grep "linux" -n --color |head -1| cut -d "=" -f 3 | cut -d '"' -f 2)"
+curl -L0 -s $GO_URL$GO_ENDPOINT -o go.tar.gz
+sudo tar -C /usr/local -xvf go.tar.gz
+rm go.tar.gz
+export PATH=$PATH:/usr/local/go/bin >> ~/.profile
