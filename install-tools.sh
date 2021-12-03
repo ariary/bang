ea88f88b97b03061ef58616621a8e1f99628c62b
Ariary <ariary9.2@hotmail.fr>
2021-12-03 15:00:06 +0100

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -2,4 +2,16 @@
 
 ## Install tools
 
-#docker, xclip,etc 
+#docker, go,etc
+
+#xclip
+sudo apt install xclip
+
+#bat & extras
+sudo apt install bat
+git clone https://github.com/eth-p/bat-extras.git && cd bat-extras && ./build.sh
+mv ./bin/* ~/.local/bin/
+
+#fd
+sudo apt install fd-find
+ln -s $(which fdfind) ~/.local/bin/fd
