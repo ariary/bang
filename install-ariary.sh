16d011c28e9c964d01d3521112a4dc13b1104e07
Ariary <ariary9.2@hotmail.fr>
2021-12-03 15:02:03 +0100

    Update install-ariary.sh

--- install-ariary.sh
+++ install-ariary.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
  
 ## Install and update ariary repo
-## put ~/bin in your $PATH (export $PATH=$PATH:$HOME/bin
+## put ~/.local/bin/ in your $PATH (export $PATH=$PATH:$HOME/.local/bin/)
 
 ### Queensono
 echo "[*] Install Queensono.."
 curl -s -lO -L https://github.com/ariary/QueenSono/releases/latest/download/qsreceiver
 curl -s -lO -L https://github.com/ariary/QueenSono/releases/latest/download/qssender
 chmod +x qsreceiver qssender
-mv qsreceiver $HOME/bin && mv qssender $HOME/bin
+mv qsreceiver $HOME/.local/bin/ && mv qssender $HOME/.local/bin/
 
 
 ### Fileless-xec
 echo "[*] Install fileless-xec.."
 curl -s -lO -L https://github.com/ariary/fileless-xec/releases/latest/download/fileless-xec
 chmod +x fileless-xec
-mv fileless-xec $HOME/bin
+mv fileless-xec $HOME/.local/bin/
 
 
 ### Gitar
 echo "[*] Install gitar.."
 curl -s -lO -L https://github.com/ariary/gitar/releases/latest/download/gitar
 chmod +x gitar
-mv gitar $HOME/bin
+mv gitar $HOME/.local/bin/
