490406dce0ed5ee50ec29280488879fd232ee844
Ariary <ariary9.2@hotmail.fr>
2021-12-07 08:40:35 -0500

    Update post-install-zsh.sh

--- install/post-install-zsh.sh
+++ install/post-install-zsh.sh
@@ -2,4 +2,4 @@
 
 echo "[*] Install ZSH theme"
 sed -i '/ZSH_THEME/c\ZSH_THEME="afowler"' ~/.zshrc
-sed -i "/PROMPT=/c\PROMPT=\'%{\$fg_bold[green]%}%m%{\$reset_color%} %{\${fg_bold[blue]}%}:: %{\$reset_color%}%{\${fg[green]}%}%3~ \$(git_prompt_info)%{\${fg_bold[\$CARETCOLOR]}%}»%{\${reset_color}%} \'" ~/.oh-my-zsh/themes/afowler.zsh-theme
+sed -i "/PROMPT=/c\PROMPT=\'%{\$fg_bold[green]%}🦍 %m%{\$reset_color%} %{\${fg_bold[blue]}%}:: %{\$reset_color%}%{\${fg[green]}%}%3~ \$(git_prompt_info)%{\${fg_bold[\$CARETCOLOR]}%}»%{\${reset_color}%} \'" ~/.oh-my-zsh/themes/afowler.zsh-theme
