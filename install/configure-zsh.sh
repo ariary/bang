9b0e7de49bb6afd1d102ce7b08e8abc6d7a1e149
Ariary <ariary9.2@hotmail.fr>
2022-02-01 09:12:17 -0500

    Update configure-zsh.sh

--- install/configure-zsh.sh
+++ install/configure-zsh.sh
@@ -11,3 +11,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo -e "${BLUE}[*] Change zsh theme..${NC}"
 sed -i '/ZSH_THEME/c\ZSH_THEME="afowler"' ~/.zshrc
 sed -i "/PROMPT=/c\PROMPT=\'%{\$fg_bold[green]%}🦍 %m%{\$reset_color%} %{\${fg_bold[blue]}%}:: %{\$reset_color%}%{\${fg[green]}%}%3~ \$(git_prompt_info)%{\${fg_bold[\$CARETCOLOR]}%}»%{\${reset_color}%} \'" ~/.oh-my-zsh/themes/afowler.zsh-theme
+
+echo "Restart zsh to make changes effective"
