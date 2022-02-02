8da0f3cc700d8206057eec59f67a1c1b2f176ff4
Ariary <ariary9.2@hotmail.fr>
2022-02-02 13:59:28 +0100

    Delete post-install-zsh.sh

--- install/post-install-zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export BLUE='\033[0;34m'
-export NC='\033[0m'
-
-echo -e "${BLUE}[*] Install ZSH theme${NC}"
-sed -i '/ZSH_THEME/c\ZSH_THEME="afowler"' ~/.zshrc
-sed -i "/PROMPT=/c\PROMPT=\'%{\$fg_bold[green]%}🦍 %m%{\$reset_color%} %{\${fg_bold[blue]}%}:: %{\$reset_color%}%{\${fg[green]}%}%3~ \$(git_prompt_info)%{\${fg_bold[\$CARETCOLOR]}%}»%{\${reset_color}%} \'" ~/.oh-my-zsh/themes/afowler.zsh-theme
